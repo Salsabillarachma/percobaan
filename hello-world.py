7216ef0f7ca7d3340efd24662d70acd3c5163154
Salsabillarachma <91858571+Salsabillarachma@users.noreply.github.com>
2023-11-17 23:26:15 +0700

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,15 +1,19 @@
 import pandas as pd
-import streamlit as st 
- 
-df = pd.DataFrame({
-    'c1': [1, 2, 3, 4],
-    'c2': [10, 20, 30, 40],
-})
- 
-st.dataframe(data=df, width=500, height=150)
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from babel.numbers import format_currency
+sns.set(style='dark')
 
-df = pd.DataFrame({
-    'c1': [1, 2, 3, 4],
-    'c2': [10, 20, 30, 40],
-})
-st.table(data=df)
+def create_daily_orders_df(df):
+    daily_orders_df = df.resample(rule='D', on='order_date').agg({
+        "order_id": "nunique",
+        "total_price": "sum"
+    })
+    daily_orders_df = daily_orders_df.reset_index()
+    daily_orders_df.rename(columns={
+        "order_id": "order_count",
+        "total_price": "revenue"
+    }, inplace=True)
+    
+    return daily_orders_df
