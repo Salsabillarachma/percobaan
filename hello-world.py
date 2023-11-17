519973c9fefc19de1335ea5020576c32c5deb913
Salsabillarachma <91858571+Salsabillarachma@users.noreply.github.com>
2023-11-17 17:19:41 +0700

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,8 +1,9 @@
+import pandas as pd
 import streamlit as st 
  
-st.write(
-    """
-    # My first app
-    Hello, para calon praktisi data masa depan!
-    """
-)
+df = pd.DataFrame({
+    'c1': [1, 2, 3, 4],
+    'c2': [10, 20, 30, 40],
+})
+ 
+st.dataframe(data=df, width=500, height=150)
